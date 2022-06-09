f0d479c0cbcca5a846242de769fefe99c3479965
MirkoTh <mirkothalmann@hotmail.com>
2022-06-09 17:36:50 +0200

    blueprint for stan model

---
+++ R/scripts/gcm.R
@@ -0,0 +1,117 @@
+library(cmdstanr)
+library(rutils)
+library(tidyverse)
+
+dir_home_grown <- c("R/utils/ellipse-utils.R")
+walk(dir_home_grown, source)
+
+check_cmdstan_toolchain(fix = TRUE, quiet = TRUE)
+check_cmdstan_toolchain()
+
+
+# Create Categorization Data ----------------------------------------------
+l_info <- list(
+  n_stimuli = 100, n_categories = 2, category_shape = "ellipses", 
+  cat_type = "exemplar", is_reward = FALSE
+)
+l_tmp <- make_stimuli(l_info)
+tbl <- l_tmp[[1]]
+l_info <- l_tmp[[2]]
+n_trials_per_stim <- 4
+tbl <- tbl %>% mutate(
+  x1 = scale(x1)[, 1],
+  x2 = scale(x2)[, 1],
+  n_correct = ceiling(exp(-(abs(x1) + abs(x2))) * n_trials_per_stim),
+  n_trials = n_trials_per_stim
+)
+tbl$n_correct[tbl$category == 1] <- abs(tbl$n_correct[tbl$category == 1] - max(tbl$n_correct))
+ggplot(tbl, aes(x1, x2, group = category)) +
+  geom_raster(aes(fill = category, alpha = n_correct)) +
+  guides(fill = "none") +
+  theme_bw()
+
+
+# compute pairwise distances
+m_distances_x1 <- map(1:nrow(tbl), distance_1d, tbl, "x1") %>% unlist() %>%
+  matrix(byrow = FALSE, nrow = nrow(tbl), ncol = nrow(tbl))
+m_distances_x2 <- map(1:nrow(tbl), distance_1d, tbl, "x2") %>% unlist() %>%
+  matrix(byrow = FALSE, nrow = nrow(tbl), ncol = nrow(tbl))
+
+# Run Stan Model ----------------------------------------------------------
+
+
+stan_binomial <- write_stan_file("
+data {
+  int n_stim;
+  array[n_stim] int n_trials; // n trials
+  array[n_stim] int n_correct; // correct categorization responses
+  array[n_stim, 2] real x;
+  array[n_stim, n_stim] real<lower=0> d;
+}
+parameters {
+  array[n_stim] real<lower=0,upper=1> theta;
+  array[n_stim] real mu;
+  real <lower=0> sigma_stimulus;
+}
+
+model {
+  n_correct ~ binomial(n_trials, theta);
+  logit(theta) ~ normal(mu, sigma_stimulus);
+  mu ~ normal(0, 1);
+  sigma_stimulus ~ uniform(0, 10);
+  
+  // Similarities
+  for (i in 1:nstim){
+    for (j in 1:nstim){
+      s[i,j] <- exp(-c*(w*d1[i,j]+(1-w)*d2[i,j]))
+    }
+  }
+}
+
+generated quantities {
+  array[n_stim] int n_correct_predict;
+  n_correct_predict = binomial_rng(n_trials, theta);
+
+}
+
+")
+mod <- cmdstan_model(stan_binomial)
+vars <- mod$variables()
+names(vars$data)
+mod$exe_file()
+
+l_data <- list(
+  n_stim = nrow(tbl), n_trials = tbl$n_correct, n_correct = tbl$n_correct, 
+  x = as.matrix(tbl[, c("x1", "x2")]),
+  d1 = m_distances_x1, d2 = m_distances_x2
+)
+
+
+fit <- mod$sample(data = l_data)
+tbl_summary <- fit$summary()
+tbl_draws <- fit$draws(format = "df")
+tbl_posterior <- tbl_draws %>% 
+  select(mu, .chain) %>% 
+  rename(chain = .chain) %>%
+  pivot_longer(mu, names_to = "parameter", values_to = "value")
+kd <- rutils::estimate_kd(tbl_posterior, "mu")
+
+params_bf <- c("mu")
+l <- sd_bfs(tbl_posterior, params_bf, sqrt(2)/4)
+#l[[2]]$value[l[[2]]$variable == "thxhi_x"] <- 1
+rutils::plot_posterior("mu", tbl_posterior, l[[2]])
+
+
+
+
+
+
+
+
+tbl_long <- tbl_draws[, c("theta[1]", "theta[15]", "theta[35]", "theta[36]")] %>% 
+  pivot_longer(c("theta[1]", "theta[15]", "theta[35]", "theta[36]"), names_to = "param")
+ggplot(tbl_long, aes(value, group = param)) +
+  geom_density(aes(color = param)) +
+  scale_color_brewer(palette = "Set1")
+
+
