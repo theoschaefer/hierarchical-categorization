4037575487575bf6f9b1bd2b0f75534399506053
MirkoTh <mirkothalmann@hotmail.com>
2022-10-05 13:06:24 +0200

    run gcm inference pipeline and plot results

--- R/scripts/feature-inference.R
+++ R/scripts/feature-inference.R
@@ -11,13 +11,11 @@ utils_loc <- c("R/utils/plotting-utils.R", "R/utils/utils.R")
 walk(utils_loc, source)
 
 
-
 # Load Data and Preprocess Them -------------------------------------------
 
 tbl_both <- readRDS(file = "data/infpro_task-cat_beh/tbl_both.RDS")
 tbl_train <- readRDS(file = "data/infpro_task-cat_beh/tbl_train.RDS")
 tbl_transfer <- readRDS(file = "data/infpro_task-cat_beh/tbl_transfer.RDS")
-
 
 # params to revert back to untransformed space
 mean_d1i <- mean(tbl_both$d1i)
@@ -26,8 +24,9 @@ mean_d2i <- mean(tbl_both$d2i)
 sd_d2i <- sd(tbl_both$d2i)
 
 
-p_id <- 101
+# example using category learning training stimuli ------------------------
 
+p_id <- 101
 file_loc_gcm <- str_c("data/infpro_task-cat_beh/models/gcm-model-", p_id, ".RDS")
 file_loc_gaussian <- str_c(
   "data/infpro_task-cat_beh/models/gaussian-model-", p_id, ".RDS"
@@ -50,7 +49,8 @@ l_tbl_exemplars <- tbl_train %>% filter(participant == p_id) %>%
   count() %>% select(-n) %>%
   split(.$category)
 
-# this has to be replaced once completion data are available
+# just uses cat learn training data to get a set of inference cues
+# has to be replaced once completion data are available
 # as exemplars will contain the cue from one dimension
 # plus a fine grid over plausible category values from the second dimension
 
@@ -72,6 +72,7 @@ tbl_cat_dim <- crossing(i_cat = ids_cat, i_dim = ids_dim)
 l_closest <- pmap(
   tbl_cat_dim, 
   max_sim_responses, 
+  post_c = post_c,
   l_tbl_lookup = l_tbl_lookup,
   l_tbl_exemplars = l_tbl_exemplars
 )
@@ -80,9 +81,45 @@ tbl_closest$d1i_z * sd_d1i + mean_d1i
 
 
 
+# implementation using empirical inference data ---------------------------
+
+
 tbl_completion_prep <- read_csv(file = "data/infpro_task-cat_beh/sub-all_task-inf_beh-distances.csv")
-cols_required <- c("participant", "category", "rep", "cuedim", "cue_val", "respdim", "resp_i")
+cols_required <- c(
+  "participant", "category", "rep", "cuedim", "cue_val",
+  "respdim", "resp_i", "representation", "distance"
+  )
 tbl_completion <- tbl_completion_prep[, cols_required] %>%
   group_by(participant, category, cuedim, respdim, cue_val, rep) %>%
-  mutate(rwn = row_number(resp_i)) %>%
-  filter(rwn == 1) %>% ungroup()
+  filter(representation == "prototype_phys") %>% ungroup() %>%
+  select(-representation)
+
+n_workers_available <- parallel::detectCores()
+plan(multisession, workers = n_workers_available / 2)
+safe_distances <- safely(distance_from_model_based_inferene)
+
+p_ids <- sort(unique(tbl_completion_prep$participant))
+l_results <- future_map(
+  p_ids, 
+  safe_distances, 
+  tbl_completion = tbl_completion, 
+  tbl_train = tbl_train, 
+  .progress = TRUE
+)
+
+# ok
+l_gcm_results <- map(l_results, "result")
+# not ok
+map(l_results, "error") %>% reduce(c)
+
+tbl_gcm_results <- l_gcm_results %>% reduce(rbind)
+tbl_gcm_results$distance_pt_phys <- tbl_completion$distance
+tbl_gcm_results %>% 
+  mutate(distance = abs(distance)) %>%
+  rename(GCM = distance, `Physical PT` = distance_pt_phys) %>%
+  pivot_longer(c(GCM, `Physical PT`)) %>% 
+  ggplot(aes(value, group = name)) +
+  geom_freqpoly(aes(color = name)) +
+  theme_bw() +
+  scale_color_brewer(palette = "Set1", name = "Distance From") +
+  labs(x = "Distance", y = "Nr. Responses")
